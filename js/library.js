0ca62d51f07a00d3a9484d2d6f20bbc7d5c0e137
Justin Duong <jduong@umich.edu>
2023-03-29 20:03:15 -0400

    Finished project.

--- js/library.js
+++ js/library.js
@@ -1,3 +1,34 @@
+let myLibrary = [
+  new Book('And Then There Were None', 'Agatha Christie', '1939-11-06', 272, ['Mystery', 'Crime', 'Psychological Thriller'], true),
+  new Book('War and Peace', 'Leo Tolstoy', '1869-01-01', 1225, ['Historical'], true),
+  new Book("The High King's Golden Tongue", 'Megan Derr', '2012-05-01', 41, ['M M Romance', 'Historical', 'Fantasy'], false),
+];
+
+function Book(title, author, publicationDate, pages, genres, read) {
+  this.title = title;
+  this.author = author;
+  this.publicationDate = publicationDate;
+  this.pages = pages;
+  this.genres = genres;
+  this.read = read;
+
+
+  this.deleteBook = function(idx) {
+    myLibrary.splice(idx, 1);
+    arrangeLibrary(myLibrary);
+  }
+
+  this.toggleRead = function() {
+    this.read = !this.read;
+    arrangeLibrary(myLibrary);
+  }
+
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
 function arrangeLibrary(library) {
   const divBooks = document.getElementById('div-books');
 
@@ -7,6 +38,7 @@ function arrangeLibrary(library) {
   for (let i = 0; i < library.length; i++) {
     const book = library[i];
     const cardDiv = document.createElement('div');
+    cardDiv.setAttribute('data-index', `${i}`);
     cardDiv.classList.add('div-card');
 
     const titleHeader = document.createElement('h2');
@@ -15,27 +47,78 @@ function arrangeLibrary(library) {
 
     const bookAuthor = document.createElement('span');
     bookAuthor.classList.add('book-author');
-    bookAuthor.innerHTML = book.author;
+    bookAuthor.innerHTML = `Author: ${book.author}`;
 
     const publicationDate = document.createElement('span');
     publicationDate.classList.add('book-publication-date');
-    publicationDate.innerHTML = book.publicationDate;
+    publicationDate.innerHTML = `Publication Date: ${book.publicationDate}`;
 
     const pages = document.createElement('span');
     pages.classList.add('book-pages');
-    pages.innerHTML = book.pages;
+    pages.innerHTML = `Pages: ${book.pages}`;
 
     const genreList = document.createElement('ul');
     genreList.classList.add('ul-genres');
-    for (let j = 0; j < book.genre; j++) {
-      const genreName = book.genre[j];
+    for (let j = 0; j < book.genres.length; j++) {
+      const genreName = book.genres[j];
       const listItem = document.createElement('li');
       listItem.innerHTML = genreName;
       genreList.append(listItem);
     }
 
-    cardDiv.append(titleHeader, bookAuthor, publicationDate, pages, genreList);
+    const isRead = document.createElement('span');
+    isRead.classList.add('book-read');
+    isRead.innerHTML = `Read: ${book.read}`;
+
+    const divButtons = document.createElement('div');
+    divButtons.classList.add('div-buttons');
+
+    const buttonDelete = document.createElement('button');
+    buttonDelete.classList.add('btn-delete');
+    buttonDelete.innerHTML = 'Delete';
+
+    buttonDelete.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const divCard = e.currentTarget.parentElement.parentElement;
+
+      const dataIndex = divCard.getAttribute('data-index');
+      myLibrary[dataIndex].deleteBook(dataIndex);
+    });
+
+    const buttonRead = document.createElement('button');
+    buttonRead.classList.add('btn-read');
+    buttonRead.innerHTML = 'Read';
+
+    buttonRead.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const divCard = e.currentTarget.parentElement.parentElement;
+
+      const dataIndex = divCard.getAttribute('data-index');
+
+      if (buttonRead.classList.contains('read')) {       
+        myLibrary[dataIndex].toggleRead();
+        buttonRead.classList.remove('read');
+      } else {
+        myLibrary[dataIndex].toggleRead();
+        buttonRead.classList.add('read');
+      }
+    });
+
+    divButtons.append(buttonDelete, buttonRead);
+
+    cardDiv.append(titleHeader, bookAuthor, publicationDate, pages, genreList, isRead, divButtons);
     divBooks.append(cardDiv);
+  }
+}
+
+function clearInputs() {
+  const formNewEntry = document.getElementById('form-new-book');
+  const formElements = formNewEntry.elements;
+
+  for (let i = 0; i < 5; i++) {
+    formElements[i].value = '';
   }
 }
 
@@ -44,70 +127,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const formNewEntry = document.getElementById('form-new-book');
   const divForm = document.getElementById('div-form');
 
-  let myLibrary = [
-    {
-      title: 'And Then There Were None',
-      author: 'Agatha Christie',
-      publicationDate: '1939-11-06',
-      pages: 272,
-      genre: ['Mystery', 'Crime', 'Psychological Thriller']
-    },
-    {
-      title: 'War and Peace',
-      author: 'Leo Tolstoy',
-      publicationDate: '1869-01-01',
-      pages: 1225,
-      genre: ['Historical']
-    },
-    {
-      title: "The High King's Golden Tongue",
-      author: 'Megan Derr',
-      publicationDate: '2012-05-01',
-      pages: 41,
-      genre: ['M M Romance', 'Historical', 'Fantasy']
-    }
-  ];
-
   arrangeLibrary(myLibrary);
-
-  function Book() {
-    // constructor...
-  }
-
-  function addBookToLibrary(book) {
-    // Do stuff here...
-    myLibrary.push(book);
-
-
-  }
 
   buttonNewBook.addEventListener('click', (e) => {
     e.preventDefault();
 
-    // open form modal.
     divForm.classList.remove('hide');
   });
 
-  // Grab Form entry, make new book object and push into myBooks array.
   formNewEntry.addEventListener('submit', (e) => {
     e.preventDefault();
     const formElements = formNewEntry.elements;
-    const newBook = {};
-
-    for (let i = 0; i < formElements.length; i++) {
-      const element = formElements[i];
-      
-      if (element.tagName === 'button') {
-        continue;
-      }
-
-      newBook[element.name] = element.value;
-    }
-
+    const newBook = new Book(formElements[0].value, formElements[1].value, formElements[2].value, formElements[3].value, formElements[4].value);
+    
     addBookToLibrary(newBook);
     arrangeLibrary(myLibrary);
-    console.log(myLibrary);
+    clearInputs();
+    divForm.classList.add('hide');
   });
-
-
 });
